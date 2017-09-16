383713c2fdc462fb7e1ec639c659dfb9158f1a2e
guldraz <gfkurtz@gmail.com>
2017-09-16 10:26:11 -0300

    Primeira Versao

--- VoceEAsMinas/src/VoceEAsMinas.java
+++ VoceEAsMinas/src/VoceEAsMinas.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
@@ -24,19 +28,59 @@ public class VoceEAsMinas {
 
 		System.out.println("\n--- Inicializando Matriz de Minas para arquivo " + arquivo + " ---");
 		
+		boolean[][] campo = new boolean[1][1];
+		boolean[] temMinaX;
+		boolean[] temMinaY;
+		
+		try (BufferedReader br = Files.newBufferedReader(input, Charset.forName("utf8")))
+		{
+			String linha = br.readLine();
+			Scanner sc = new Scanner(linha).useDelimiter(" ");
+			
+			System.out.println(linha);
+			
+			int larguraTerreno = sc.nextInt();
+			System.out.println("Largura do Terreno: " + larguraTerreno);
+			int alturaTerreno = sc.nextInt();
+			System.out.println("Altura do Terreno: " + alturaTerreno);
+			int numeroMinas = sc.nextInt();
+			System.out.println("Número de Minas: " + numeroMinas);
+			long totalCoord = Long.valueOf(larguraTerreno) * Long.valueOf(alturaTerreno);
+			System.out.println("Total de Coordenadas no Terreno: " + totalCoord);
+			double gbMemoria = totalCoord/1073741824.0;
+			System.out.println("Consumo de Memória: " + Math.round(gbMemoria*100)/100.0 + "GB");
+			
+			// Inicia matriz do tamanho do terreno com todos os pontos falsos			
+			campo = new boolean[larguraTerreno][alturaTerreno];
+			
+			//Vetores para indicar se ha minas em uma determinada Linha ou Coluna			
+			temMinaX = new boolean[larguraTerreno];
 
+			temMinaY = new boolean[alturaTerreno];
+			
+			
+			//Populando campo com as minas do arquivo e informações relevantes
+			int minaX, minaY;
+			while((linha = br.readLine()) != null) {
+				sc = new Scanner(linha).useDelimiter(" ");
+								
+				minaX = sc.nextInt() - 1;
+				minaY = sc.nextInt() - 1;
+				
+				campo[minaX][minaY] = true;
+				temMinaX[minaX] = true;
+				temMinaY[minaY] = true;
+				
+			}
+				
+		}
+		catch (IOException x) {
+			System.err.format("Erro de E/S: %s%n", x);
+		}
 		
-		System.out.println("\n--- Adaptacao concluida ---"
-				+ "\n\n--- Realizando contagem de palavras para Lista Remissiva ---");
 		
-		Path pathFormatado = Paths.get("textoAdaptado.txt");
-		
-		contador = new ContadorDePalavras(pathFormatado, lista);
-		contador.contar();
-		
-		System.out.println("\n--- Contagem finalizada com sucesso ---");
+		System.out.println("\n--- Matriz do Campo inicializada com sucesso ---");
 	
-		
 	}
 	
 }
